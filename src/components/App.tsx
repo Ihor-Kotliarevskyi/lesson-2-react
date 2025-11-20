b4ab6fe07ab059f3c1ae04a654581a414370c034
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-11-20 21:43:04 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import Product from "./Product";
+
+
+export default function App() {
+  return (
+    <>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
