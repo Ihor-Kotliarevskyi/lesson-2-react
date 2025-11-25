4a6c0ba43c462be6fc6193dc25c4426f0462c6a9
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-11-25 21:22:39 +0200

    Changes to be committed: #	modified:   src/components/App.tsx #	new file:   src/components/ClickCounter.tsx #	deleted:    src/components/Product.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,33 @@
-import Product from "./Product";
+import ClickCounter from "./ClickCounter";
+import { useState } from "react";
 
+interface Values {
+  x: number;
+  y: number;
+  z: number;
+}
 
 export default function App() {
+  const [values, setValues] = useState<Values>({ x: 0, y: 0, z: 0 });
+
+  const updateValue = (key: keyof Values) => {
+    setValues({
+      ...values,
+      [key]: values[key] + 1,
+    });
+  };
+
   return (
     <>
-      <h1>Products</h1>
-
-      <Product />
-      <Product />
-      <Product />
+      <ClickCounter />
+      <div>
+        <p>
+          x: {values.x}, y: {values.y}, z: {values.z}
+        </p>
+        <button onClick={() => updateValue("x")}>Update x</button>
+        <button onClick={() => updateValue("y")}>Update y</button>
+        <button onClick={() => updateValue("z")}>Update z</button>
+      </div>
     </>
   );
 }
